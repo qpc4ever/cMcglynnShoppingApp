ec32c8d9b18737ce2b92ca93049ea9cf583fd335
qpc4ever <qpc4ever@gmail.com>
2017-08-17 16:40:56 -0400

    Initial commit

---
+++ cMcglynnShoppingApp/Migrations/Configuration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using cMcglynnShoppingApp.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace cMcglynnShoppingApp.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        public object New { get; private set; }
+
+        protected override void Seed(ApplicationDbContext context)
+        {
+            var roleManager = new RoleManager<IdentityRole>(
+                new RoleStore<IdentityRole>(context));
+            if (!context.Roles.Any(r => r.Name == "Admin"))
+            {
+
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+            }
+            var userManager = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(context));
+            if (!context.Users.Any(u => u.Email == "qpc4ever@gmail.com"))
+            {
+                userManager.Create(new ApplicationUser
+                {
+                    UserName = "qpc4ever@gmail.com",
+                    Email = "qpc4ever@gmail.com",
+                    FirstName = "Chris",
+                    LastName = "McGlynn",
+                }, "Qpc4ever!");
+            }
+            var userId = userManager.FindByEmail("qpc4ever@gmail.com").Id;
+            userManager.AddToRole(userId, "Admin");
+        }
+    }
+}
---
+++ cMcglynnShoppingApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cMcglynnShoppingApp.Startup))]
+namespace cMcglynnShoppingApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
